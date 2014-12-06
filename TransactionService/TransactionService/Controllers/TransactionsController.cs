adacaa8fa78641104d570aef5365204624487bdd
ZoltarPoD <tylermcconville@gmail.com>
2014-12-06 12:52:51 -0600

    Created a database to store transactions, created a TransactionsController with the ability to read transactions, and implemented a LINQ-to-SQL data context for easier communication with the database

---
+++ TransactionService/TransactionService/Controllers/TransactionsController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+using TransactionService.Data_Access;
+
+namespace TransactionService.Controllers
+{
+    public class TransactionsController : ApiController
+    {
+        public IHttpActionResult GetTransaction(int id)
+        {
+            using (var context = new TransactionDataContext())
+            {
+                var transaction = context.Transactions.FirstOrDefault(x => x.Id == id);
+
+                if (transaction == null)
+                {
+                    return NotFound();
+                }
+
+                return Ok(transaction);
+            }
+        }
+
+        public IEnumerable<Transaction> GetAllTransactions()
+        {
+            using (var context = new TransactionDataContext())
+            {
+                return context.Transactions.Select(x => x).ToList();
+            }
+        }
+    }
+}
