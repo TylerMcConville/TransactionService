ea8ab17296fdac2914fb39914b5605e622d113e4
ZoltarPoD <tylermcconville@gmail.com>
2014-12-09 19:31:23 -0600

    Including file that was missed in earlier commits/pushes

---
+++ TransactionService/TransactionService/Models/Transaction.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TransactionService.Models
+{
+    public class Transaction
+    {
+        [Required]
+        public decimal? Amount { get; set;  }
+
+        public string Description { get; set; }
+
+        [Required]
+        public bool? WasPlanned { get; set; }
+
+        [Required]
+        public bool? WasNecessary { get; set; }
+
+        public Data_Access.Transaction ToDataAccessTransaction()
+        {
+            return new Data_Access.Transaction
+            {
+                Amount = Amount.Value,
+                Description = Description,
+                WasNecessary = WasNecessary.Value,
+                WasPlanned = WasPlanned.Value
+            };
+        }
+    }
+}
